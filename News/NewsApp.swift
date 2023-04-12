3db27c7b387822cfe36668def64dfc302f169833
Biagio <biagior00@gmail.com>
2023-04-12 19:49:58 +0200

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Biagio Ricci on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
